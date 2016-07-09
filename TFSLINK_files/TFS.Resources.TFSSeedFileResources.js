310d2d4aba7ba8547974cacacaeee05d3b8612c0
pratikshami <pratikshami@cybage.com>
2016-07-09 11:01:13 +0530

    1

---
+++ TFSLINK_files/TFS.Resources.TFSSeedFileResources.js
@@ -0,0 +1,7 @@
+define(["require", "exports"], function(require, exports) {exports.ErrorPaneCloseLink="Close [x]";
+exports.ModuleScriptErrorFormat="Module \u0027{0}\u0027 cannot be loaded. This may be due to lost connectivity or an incomplete or corrupted download of the file.  Try refreshing your browser or clearing your browser cache to retrieve the file again.";
+exports.CircularDependencyErrorFormat="There is a circular dependency between TFS script modules {0} and {1}.";
+exports.UnknownServerErrorMessage="Unknown server error occured.";
+exports.UnknownErrorMessage="Unknown error.";
+exports.ErrorPaneHeader="Error(s):";
+});
